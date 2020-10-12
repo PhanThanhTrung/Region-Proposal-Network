367ed16b09a8605f8e86f0ac292eeff7f3058bf3
PhanThanhTrung <trung99nb@gmail.com>
2020-10-12 19:35:27 +0700

    add config for training and testing

---
+++ config.py
@@ -0,0 +1,9 @@
+#Data loader configuration
+train_image_folder='/home/fluoxetine/Repository/Datasets/Wider_Face/WIDER_train/'
+train_anno_file_path='/home/fluoxetine/Repository/Datasets/Wider_Face/wider_face_split/wider_face_train_bbx_gt.txt'
+val_image_folder='/home/fluoxetine/Repository/Datasets/Wider_Face/WIDER_val/'
+val_anno_file_path='/home/fluoxetine/Repository/Datasets/Wider_Face/wider_face_split/wider_face_val_bbx_gt.txt'
+
+#Region Proposal Network configuration
+number_of_anchor=9
+backbone='VGG'
